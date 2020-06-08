b1c10f698d901cec7e705fc87d19b92b240c21b8
ycdeng <402979006@qq.com>
2020-06-08 16:41:11 +0800

    Revert "Fix fed kafkasend (#36)" (#37)
    This reverts commit c293f4eb4f3a38622e678d435a3a272ad80662b1.

--- federation/federationapi/federationapi.go
+++ federation/federationapi/federationapi.go
@@ -112,10 +112,7 @@ func (fed *FederationAPIComponent) OnMessage(ctx context.Context, topic string, 
 		log.Errorf("decode error: %v", err)
 		return
 	}
-	if msg.Key == nil {
-		msg.Key = []byte{}
-	}
-	log.Infof("fed-api recv topic:%s cmd:%d key:%s", topic, msg.Cmd, string(msg.Key))
+	log.Infof("fed-api recv topic:%s cmd:%d", topic, msg.Cmd)
 
 	// call federation api by commandID
 	var retMsg *model.GobMessage
@@ -134,21 +131,21 @@ func (fed *FederationAPIComponent) OnMessage(ctx context.Context, topic string, 
 	retMsg.MsgType = model.REPLY
 	retMsg.MsgSeq = msg.MsgSeq
 	retMsg.NodeId = id.GetNodeId()
-	retMsg.Key = msg.Key
+
 	//nodeID := strings.TrimPrefix(subject, fmt.Sprintf("%s.", fed.cfg.GetMsgBusReqTopic()))
 	//resSubject := fmt.Sprintf("%s.%s", fed.cfg.GetMsgBusResTopic(), nodeID)
-	log.Infof("resSubject: %s, cmd: %d, key:%s, retMsg: %s ", fed.cfg.Kafka.Producer.FedAPIOutput.Topic, msg.Cmd, string(msg.Key), retMsg.Body)
+	log.Infof("resSubject: %s, cmd: %d, retMsg: %s", fed.cfg.Kafka.Producer.FedAPIOutput.Topic, msg.Cmd, retMsg.Body)
 
 	span, _ := common.StartSpanFromContext(ctx, fed.cfg.Kafka.Producer.FedAPIOutput.Name)
 	defer span.Finish()
 	common.ExportMetricsBeforeSending(span, fed.cfg.Kafka.Producer.FedAPIOutput.Name,
 		fed.cfg.Kafka.Producer.FedAPIOutput.Underlying)
-	common.GetTransportMultiplexer().SendAndRecvWithRetry(
+	common.GetTransportMultiplexer().SendWithRetry(
 		fed.cfg.Kafka.Producer.FedAPIOutput.Underlying,
 		fed.cfg.Kafka.Producer.FedAPIOutput.Name,
 		&core.TransportPubMsg{
 			//Format: core.FORMAT_GOB,
-			Keys:    retMsg.Key,
+			Keys:    []byte{},
 			Obj:     retMsg,
 			Headers: common.InjectSpanToHeaderForSending(span),
 		})
