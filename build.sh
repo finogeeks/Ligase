41642465f5cc280327a805d5e54293242bd88856
scp1513 <scp1513@gmail.com>
2020-07-13 09:04:58 +0800

    feat: add fed add content entry (#43)
    Co-authored-by: caojiawen <caojiawen@finogeeks.com>

--- build.sh
+++ build.sh
@@ -8,9 +8,9 @@ export GOPROXY=https://goproxy.io
 
 cd $PROJDIR/cmd/engine-server
 go build -v -o $PROJDIR/bin/engine-server
-cd $PROJDIR/federation
+cd $PROJDIR/cmd/federation
 go build -v -o $PROJDIR/bin/federation
-cd $PROJDIR/federation
+cd $PROJDIR/cmd/content
 go build -v -o $PROJDIR/bin/content
 
 cd $PROJDIR
