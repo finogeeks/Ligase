37c7c79d6331ba9985ea86a05f106366330ea490
charizer <chenzrcd@163.com>
2020-08-04 15:06:16 +0800

    add content fed entry (#48)
    * 4.8.11 and later
    
    * add fed content entry
    
    Co-authored-by: chenzhangrong <>

---
+++ cmd/content/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/finogeeks/ligase/content"
+)
+
+func main() {
+	content.Entry()
+}
--- federation/version.go
+++ federation/version.go
@@ -12,7 +12,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-package main
+package federation
 
 const (
 	VERSION = "2020.623.1"
