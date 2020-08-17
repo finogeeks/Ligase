5f8c3f4f6b6256fcc28e8150b3df0bad86f322b1
chenzhangrong <>
2020-08-17 11:22:44 +0800

    fix push sender block

--- pushsender/consumers/pushDataClient.go
+++ pushsender/consumers/pushDataClient.go
@@ -15,25 +15,21 @@
 package consumers
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"github.com/finogeeks/ligase/common/filter"
-	"io/ioutil"
-	"net"
+	"github.com/finogeeks/ligase/common/uid"
 	"net/http"
 	"sync"
 	"time"
 
 	"github.com/finogeeks/ligase/common"
 	"github.com/finogeeks/ligase/common/config"
-	"github.com/finogeeks/ligase/skunkworks/gomatrixserverlib"
 	"github.com/finogeeks/ligase/model/pushapitypes"
+	"github.com/finogeeks/ligase/skunkworks/log"
 	"github.com/finogeeks/ligase/storage/model"
 	"github.com/json-iterator/go"
 	"github.com/nats-io/nats.go"
-
-	"github.com/finogeeks/ligase/skunkworks/log"
 )
 
 var json = jsoniter.ConfigCompatibleWithStandardLibrary
@@ -46,7 +42,9 @@ type PushDataConsumer struct {
 	pushCount  *sync.Map
 	chanSize   uint32
 	msgChan    []chan *pushapitypes.PushPubContents
-	lock    	*sync.Mutex
+	idg 	   *uid.UidGenerator
+	lock       *sync.Mutex
+	httpClient *common.HttpClient
 }
 
 func NewPushDataConsumer(
@@ -60,9 +58,11 @@ func NewPushDataConsumer(
 		rpcClient: client,
 		chanSize:  16,
 		lock: new(sync.Mutex),
+		httpClient: common.NewHttpClient(),
 	}
 	s.pushCount = new(sync.Map)
-
+	idg, _ := uid.NewDefaultIdGenerator(0)
+	s.idg = idg
 	pushFilter := filter.GetFilterMng().Register("pushSender", nil)
 	s.pushFilter = pushFilter
 	return s
@@ -85,33 +85,33 @@ func (s *PushDataConsumer) cb(msg *nats.Msg) {
 		log.Errorf("PushDataConsumer cb Unmarshal err: %v", err)
 		return
 	}
-
 	idx := common.CalcStringHashCode(result.Input.RoomID) % s.chanSize
-	log.Infof("PushDataConsumer cb slot:%d roomID:%s eventID:%s", idx, result.Input.RoomID,result.Input.EventID)
+	result.Slot = idx
+	traceId, _ := s.idg.Next()
+	result.TraceId = fmt.Sprintf("%d", traceId)
+	log.Infof("traceid:%s PushDataConsumer cb slot:%d roomID:%s eventID:%s len(chan):%d", result.TraceId, idx, result.Input.RoomID,result.Input.EventID, len(s.msgChan[idx]))
 	s.msgChan[idx] <- &result
 }
 
 func (s *PushDataConsumer) startWorker(msgChan chan *pushapitypes.PushPubContents) {
 	for data := range msgChan {
-		if s.pushFilter.Lookup([]byte(data.Input.EventID)) {
-			log.Infof("PushDataConsumer startWorker lookup eventID:%s len(msgChan):%d", data.Input.EventID, len(msgChan))
-		}else{
-			log.Infof("start push roomID:%s eventID:%s len(msgChan):%d len(contents):%d", data.Input.RoomID, data.Input.EventID, len(msgChan), len(data.Contents))
-			s.pushFilter.Insert([]byte(data.Input.EventID))
-			for _, content := range data.Contents {
-				s.PushData(
-					data.Input,
-					content.Pushers,
-					data.SenderDisplayName,
-					data.RoomName,
-					content.UserID,
-					data.RoomAlias,
-					content.Action,
-					content.NotifyCount,
-					data.CreateContent,
-				)
-			}
-		}
+		s.doPushData(data)
+	}
+}
+
+func (s *PushDataConsumer) doPushData(data *pushapitypes.PushPubContents){
+	bs := time.Now().UnixNano()/1000000
+	defer func(bs int64){
+		spend := time.Now().UnixNano()/1000000 - bs
+		log.Infof("traceid:%s doPushData slot:%d roomID:%s eventID:%s len(contents):%d spend:%d", data.TraceId, data.Slot, data.Input.RoomID, data.Input.EventID, len(data.Contents), spend)
+	}(bs)
+	if s.pushFilter.Lookup([]byte(data.Input.EventID)) {
+		log.Infof("traceid:%s doPushData slot:%d lookup roomID:%s eventID:%s has exsit", data.TraceId, data.Slot, data.Input.RoomID, data.Input.EventID)
+		return
+	}
+	s.pushFilter.Insert([]byte(data.Input.EventID))
+	for _, content := range data.Contents {
+		s.pushData(data, content)
 	}
 }
 
@@ -127,14 +127,14 @@ func (s *PushDataConsumer) Start() error {
 	return nil
 }
 
-func (s *PushDataConsumer) SetPushFailTimes(pusherKey string, success bool) int {
+func (s *PushDataConsumer) SetPushFailTimes(pusher pushapitypes.Pusher , pusherKey string, success bool, traceId string) int {
+	log.Infof("traceid:%s SetPushFailTimes userId:%s deviceId:%s pusherKey:%s success:%t", traceId, pusher.UserName, pusher.DeviceID, pusherKey, success)
 	s.lock.Lock()
 	defer s.lock.Unlock()
 	if success {
 		s.pushCount.Store(pusherKey, 0)
 		return 0
 	}
-
 	if val, ok := s.pushCount.Load(pusherKey); ok {
 		count := val.(int)
 		count++
@@ -146,40 +146,15 @@ func (s *PushDataConsumer) SetPushFailTimes(pusherKey string, success bool) int 
 	}
 }
 
-func (s *PushDataConsumer) PushData(
-	input *gomatrixserverlib.ClientEvent,
-	pushers *pushapitypes.Pushers,
-	senderDisplayName,
-	roomName,
-	userID,
-	roomAlias string,
-	action *pushapitypes.TweakAction,
-	notifyCount int64,
-	createContent interface{},
+func (s *PushDataConsumer) pushData(
+	data *pushapitypes.PushPubContents,
+	pushContent *pushapitypes.PushPubContent,
 ) {
-	defer func() {
-		if e := recover(); e != nil {
-			stack := common.PanicTrace(4)
-			log.Panicf("%v\n%s\n", e, stack)
-		}
-	}()
-	for _, pusher := range pushers.Pushers {
-		var content pushapitypes.PushContent
-		err := json.Unmarshal(input.Content, &content.Content)
-		if err != nil {
-			log.Errorf("PushData Unmarshal err: %d  Event: %s", err, string(input.EventID))
-			continue
-		}
-
-		userIsTarget := false
-		if (input.StateKey != nil) && (userID == *input.StateKey) {
-			userIsTarget = true
-		}
-
+	for _, pusher := range pushContent.Pushers.Pushers {
 		var url string
 		pushChannel := "ios"
 		var pusherData interface{}
-		if v, ok := interface{}(pusher.Data).(map[string]interface{}); ok {
+		if v, ok := pusher.Data.(map[string]interface{}); ok {
 			var data map[string]interface{}
 			data = v
 			if v, ok := data["url"]; ok {
@@ -193,50 +168,15 @@ func (s *PushDataConsumer) PushData(
 		} else {
 			continue
 		}
-
-		if roomName == "" {
-			roomName = senderDisplayName
-		}
-
-		notify := pushapitypes.Notify{
-			Notify: pushapitypes.Notification{
-				EventId:           input.EventID,
-				RoomId:            input.RoomID,
-				Type:              input.Type,
-				Sender:            input.Sender,
-				SenderDisplayName: senderDisplayName,
-				RoomName:          roomName,
-				RoomAlias:         roomAlias,
-				UserIsTarget:      userIsTarget,
-				Priority:          "high",
-				Content:           content.Content,
-				Counts: pushapitypes.Counts{
-					UnRead: notifyCount,
-				},
-				Devices: []pushapitypes.Device{
-					{
-						DeviceID:  pusher.DeviceID,
-						UserName:  pusher.UserName,
-						AppId:     pusher.AppId,
-						PushKey:   pusher.PushKey,
-						PushKeyTs: pusher.PushKeyTs,
-						Data:      pusherData,
-						Tweak: pushapitypes.Tweaks{
-							Sound:     action.Sound,
-							HighLight: action.HighLight,
-						},
-					},
-				},
-				CreateEvent: createContent,
-			},
-		}
-
-		request, err := json.Marshal(notify)
-		if err != nil {
-			log.Errorw("process marshal error", log.KeysAndValues{"err", err})
+		notify := s.createNotify(data, pushContent, pusher, pusherData)
+		if notify == nil {
 			continue
 		}
-
+		request, err := json.Marshal(notify)
+		if err != nil {
+			log.Errorw("traceid:%s process marshal error", log.KeysAndValues{"traceid", data.TraceId, "err", err})
+			continue
+		}
 		if pushChannel == "ios" || pushChannel == "" {
 			if s.cfg.PushService.PushServerUrl != "" {
 				url = s.cfg.PushService.PushServerUrl
@@ -246,41 +186,87 @@ func (s *PushDataConsumer) PushData(
 				url = s.cfg.PushService.AndroidPushServerUrl
 			}
 		}
-		go s.doPush(url, request, pusher)
+		go s.doPush(url, request, pusher, data.TraceId)
 	}
 }
 
-func (s *PushDataConsumer) doPush(url string, request []byte, pusher pushapitypes.Pusher){
-	code, body, err := s.HttpRequest(url, request)
+func (s *PushDataConsumer) createNotify(data *pushapitypes.PushPubContents, pushContent *pushapitypes.PushPubContent, pusher pushapitypes.Pusher, pusherData interface{}) *pushapitypes.Notify {
+	userIsTarget := false
+	if (data.Input.StateKey != nil) && (pushContent.UserID == *data.Input.StateKey) {
+		userIsTarget = true
+	}
+	var content pushapitypes.PushContent
+	err := json.Unmarshal(data.Input.Content, &content.Content)
 	if err != nil {
-		log.Errorw("http request error", log.KeysAndValues{"content", string(request), "error", err})
+		log.Errorf("traceid:%s PushData Unmarshal err: %d  Event: %s", err, string(data.Input.EventID))
+		return nil
+	}
+	roomName := data.RoomName
+	if roomName == "" {
+		roomName = data.SenderDisplayName
+	}
+	notify := &pushapitypes.Notify{
+		Notify: pushapitypes.Notification{
+			EventId:           data.Input.EventID,
+			RoomId:            data.Input.RoomID,
+			Type:              data.Input.Type,
+			Sender:            data.Input.Sender,
+			SenderDisplayName: data.SenderDisplayName,
+			RoomName:          roomName,
+			RoomAlias:         data.RoomAlias,
+			UserIsTarget:      userIsTarget,
+			Priority:          "high",
+			Content:           content.Content,
+			Counts: pushapitypes.Counts{
+				UnRead: pushContent.NotifyCount,
+			},
+			Devices: []pushapitypes.Device{
+				{
+					DeviceID:  pusher.DeviceID,
+					UserName:  pusher.UserName,
+					AppId:     pusher.AppId,
+					PushKey:   pusher.PushKey,
+					PushKeyTs: pusher.PushKeyTs,
+					Data:      pusherData,
+					Tweak: pushapitypes.Tweaks{
+						Sound:     pushContent.Action.Sound,
+						HighLight: pushContent.Action.HighLight,
+					},
+				},
+			},
+			CreateEvent: data.CreateContent,
+		},
+	}
+	return notify
+}
+
+func (s *PushDataConsumer) doPush(url string, request []byte, pusher pushapitypes.Pusher, traceId string){
+	code, body, err := s.HttpRequest(url, request, traceId)
+	if err != nil {
+		log.Errorw("http request error", log.KeysAndValues{"traceid", traceId, "userId", pusher.UserName, "deviceId", pusher.DeviceID, "content", string(request), "error", err})
 		return
 	}
-
 	pusherKey := fmt.Sprintf("%s:%s", pusher.AppId, pusher.PushKey)
-
 	if code != http.StatusOK {
-		log.Errorw("http request error", log.KeysAndValues{"status_code", code, "response", string(body), "appId", pusher.AppId, "pushkey", pusher.PushKey, "content", string(request)})
-
-		failCount := s.SetPushFailTimes(pusherKey, false)
+		log.Errorw("http request error", log.KeysAndValues{"traceid", traceId, "status_code", code, "response", string(body), "userId", pusher.UserName, "deviceId", pusher.DeviceID,  "appId", pusher.AppId, "pushkey", pusher.PushKey, "content", string(request)})
+		failCount := s.SetPushFailTimes(pusher, pusherKey, false, traceId)
 		if failCount > s.cfg.PushService.RemoveFailTimes {
-			log.Warnf("for failed too many del appId:%s, pushKey:%s, display:%s", pusher.AppId, pusher.PushKey, pusher.DeviceDisplayName)
+			log.Warnf("traceid:%s for failed too many del userId:%s, deviceId:%s, appId:%s, pushKey:%s, display:%s", traceId, pusher.UserName, pusher.DeviceID, pusher.AppId, pusher.PushKey, pusher.DeviceDisplayName)
 			if err := s.pushDB.DeletePushersByKey(context.TODO(), pusher.AppId, pusher.PushKey); err != nil {
-				log.Errorw("delete pusher error", log.KeysAndValues{"err", err, "AppId", pusher.AppId, "PushKey", pusher.PushKey})
+				log.Errorw("delete pusher error", log.KeysAndValues{"traceid", traceId, "err", err, "userId", pusher.UserName, "deviceId", pusher.DeviceID, "AppId", pusher.AppId, "PushKey", pusher.PushKey})
 			}
 		}
 	} else {
 		//用以追踪IOS重复推送问题
-		log.Infof("push content success, appid:%s, pushkey:%s , content:%s", pusher.AppId, pusher.PushKey, string(request))
-		s.SetPushFailTimes(pusherKey, true)
-
+		log.Infof("traceid:%s push content success userId:%s, deviceId:%s appid:%s, pushkey:%s , content:%s", traceId, pusher.UserName, pusher.DeviceID, pusher.AppId, pusher.PushKey, string(request))
+		s.SetPushFailTimes(pusher, pusherKey, true, traceId)
 		var ack pushapitypes.PushAck
 		err = json.Unmarshal(body, &ack)
 		if len(ack.Rejected) > 0 {
 			for _, v := range ack.Rejected {
-				log.Warnf("for reject del pushKey:%s", v)
+				log.Warnf("traceid:%s for reject del userId:%s deviceId:%s pushKey:%s", traceId, pusher.UserName, pusher.DeviceID, v)
 				if err := s.pushDB.DeletePushersByKeyOnly(context.TODO(), v); err != nil {
-					log.Errorw("delete pusher error", log.KeysAndValues{"err", err})
+					log.Errorw("traceid:%s delete pusher userId:%s deviceId:%s pushKey:%s error", log.KeysAndValues{ "traceid", traceId, "userId", pusher.UserName, "deviceId", pusher.DeviceID, "PushKey", pusher.PushKey, "err", err})
 				}
 			}
 		}
@@ -288,42 +274,15 @@ func (s *PushDataConsumer) doPush(url string, request []byte, pusher pushapitype
 }
 
 func (s *PushDataConsumer) HttpRequest(
-	reqUrl string,
+	url string,
 	content []byte,
+	traceId string,
 ) (int, []byte, error) {
 	bs := time.Now().UnixNano() / 1000000
-	client := &http.Client{
-		Transport: &http.Transport{
-			Dial: (&net.Dialer{
-				Timeout:   30 * time.Second,
-				KeepAlive: 30 * time.Second,
-			}).Dial,
-			TLSHandshakeTimeout:   10 * time.Second,
-			ResponseHeaderTimeout: 10 * time.Second,
-			ExpectContinueTimeout: 1 * time.Second,
-			DisableKeepAlives:     true,
-		},
-		Timeout: 30 * time.Second,
-	}
-	req, err := http.NewRequest(http.MethodPost, reqUrl, bytes.NewReader(content))
-	if err != nil {
-		return -1, nil, err
-	}
-
-	req.Close = true
-	req.Header.Set("Content-Type", "application/json")
-
-	resp, err := client.Do(req)
-	defer func() {
-		if resp != nil && resp.Body != nil {
-			resp.Body.Close()
-		}
-	}()
-	spend := time.Now().UnixNano()/1000000 - bs
-	log.Infof("post http to push-service spend:%d", spend)
-	if err != nil {
-		return -1, nil, err
-	}
-	b, _ := ioutil.ReadAll(resp.Body)
-	return resp.StatusCode, b, err
+	defer func(bs int64){
+		spend := time.Now().UnixNano() / 1000000 - bs
+		log.Infof("traceid:%s post req to %s spend:%d", traceId, url, spend)
+	}(bs)
+	resp, err := s.httpClient.Post(url, content)
+	return resp.StatusCode(), resp.Body(), err
 }
